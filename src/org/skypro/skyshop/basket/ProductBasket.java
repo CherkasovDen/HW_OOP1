748ebd5752f34b19808549ebaa0de7c1f162e736
CherkasovDen <cherkasovdengen@gmail.com>
2025-04-07 16:22:29 +0300

    HomeWork 1

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,65 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private Product[] productsBasket = new Product[5];
+
+
+    // Наполенение корзины
+    public void addProduct(Product product) {
+        for (int i = 0; i < productsBasket.length; i++) {
+            if (productsBasket[i] == null) {
+                productsBasket[i] = product;
+                return;
+            }
+        }
+        System.out.println("Невозможно добавить продукт.");
+    }
+
+    // Общая стоимость корзины
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (Product product : productsBasket) {
+            if (product != null) {
+                totalPrice += product.getPrice();
+            }
+        }
+        return totalPrice;
+    }
+
+    // Печать содержимого корзины
+    public void printBasket() {
+        boolean isEmpty = true;
+        for (Product product : productsBasket) {
+            if (product != null) {
+                System.out.println(product.getNameProduct() + ": " + product.getPrice());
+                isEmpty = false;
+            }
+        }
+        if (isEmpty) {
+            System.out.println("В корзине пусто.");
+            return;
+        }
+        System.out.println("Итого: " + getTotalPrice());
+    }
+
+    // Проверка продукта в корзине
+    public boolean checkProduct(String nameProduct) {
+        for (Product product : productsBasket) {
+            if (product != null && product.getNameProduct().equals(nameProduct)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Очистка корзины
+        public void clearBasket1() {
+        for (int i = 0; i < productsBasket.length; i++) {
+            productsBasket[i] = null;
+        }
+    }
+
+
+}
