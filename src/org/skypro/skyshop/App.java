126e69cfcd334b99f9a3ea3ea198ed87fbe1c61a
CherkasovDen <cherkasovdengen@gmail.com>
2025-04-09 09:40:46 +0300

    HomeWork 1, changes 1

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -17,9 +17,7 @@ public class App {
         Product meat = new Product("Мясо", 400);
 
         ProductBasket bas1 = new ProductBasket();
-        /** addProduct() Метод добавления продукта в корзину: метод принимает в себя продукт и ничего не возвращает
-         *
-         */
+
         bas1.addProduct(apple);
         bas1.addProduct(milk);
         bas1.addProduct(bread);
@@ -30,38 +28,22 @@ public class App {
         bas1.addProduct(meat);
         System.out.println(" ");
 
-        /** getTotalPrice() Метод получения общей стоимости корзины: метод ничего не принимает и возвращает целое число.
-         *
-         */
+
         System.out.println("Общая стоимость корзины: " + bas1.getTotalPrice());
         System.out.println(" ");
 
-/** printBasket() Метод, который печатает содержимое корзины: метод ничего не принимает и не возвращает, но печатает в консоль сообщение вида:
- <имя продукта>: <стоимость>
- <имя продукта>: <стоимость>
- <имя продукта>: <стоимость>
- Итого: <общая стоимость корзины>
- Если в корзине ничего нет, нужно напечатать фразу «в корзине пусто».
- *
- */
+
         System.out.println("Содержимое корзины");
         bas1.printBasket();
         System.out.println(" ");
 
-/** checkProduct Метод, проверяющий продукт в корзине по имени: метод принимает в себя строку имени и возвращает
- boolean
- в зависимости от того, есть продукт в корзине или его нет.
- *
- */
+
         System.out.println("Поиск товара, который есть в корзине");
         System.out.println("В корзине есть 'Яблоко': " + bas1.checkProduct("Яблоко"));
         System.out.println("Поиск товара, которого нет в корзине.");
         System.out.println("В корзине есть 'Греча': " + bas1.checkProduct("Греча"));
         System.out.println(" ");
-/** clearBasket Метод очистки корзины: метод ничего не принимает и очищает массив, проставляя всем его элементам
- null
- *
- */
+
         System.out.println("Очистка корзины.");
         bas1.clearBasket();
 
